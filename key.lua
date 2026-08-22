3c679fb9725484964f39bea3bcc0aab9f2da49ce
CuteLing-XB <3529494476@qq.com>
2026-08-22 12:30:35 +0800

    Create key.lua

---
+++ key.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "XQVIP"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bdf69b51533c9dd2cc0d2047be9d890099b0cb3d1ab2c42f95692c07200b642b/download"))()
